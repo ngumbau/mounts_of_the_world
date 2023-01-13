869523023bf0f65b8424af0a6ba2bc2dee8148f6
Kristopher Kivutha <kkibwana@gmail.com>
2023-01-13 19:13:43 +0300

    scaffolding the mounts page

--- lib/pages/mounts_page.dart
+++ lib/pages/mounts_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:mounts_of_the_world/util/color.dart';
 
 class MountsPage extends StatelessWidget {
   const MountsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(),
+      drawer: const Drawer(),
+      body: const Center(
         child: Text('Welcome to Mounts of the World!'),
       ),
     );
