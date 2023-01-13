ade4463ea74beb7ae81ab1dbafad6ae436cc6d4d
Kristopher Kivutha <kkibwana@gmail.com>
2023-01-13 19:01:14 +0300

    added dummy landing screen

---
+++ lib/pages/mounts_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MountsPage extends StatelessWidget {
+  const MountsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Welcome to Mounts of the World!'),
+      ),
+    );
+  }
+}
